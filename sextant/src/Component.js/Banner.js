3df75a8d661695fef6aa2e66d013c4943fdd759e
AnirbanBall <ballanirban58@gmail.com>
2023-01-02 18:43:52 +0530

    adding sextant folder

---
+++ sextant/src/Component.js/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './Header' 
+
+const Banner = () => {
+  return (
+    <div className='banner'>
+     <Header />   
+    </div>
+  )
+}
+
+export default Banner
---
+++ sextant/src/Component.js/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+     <div className='logo'>SEXTANT</div>
+     <div className='Nevbar'>
+          <a href=''>Home</a>
+          <a href=''>About</a>
+          <a href=''>Solution</a>    
+          <a href=''>Contact</a>
+    </div>    
+
+    </div>
+  )
+}
+
+export default Header
